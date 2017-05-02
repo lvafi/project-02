3f1cef6e78020ea89b12660ecb3f1e8eea81e098
vf <l_vafi@yahoo.com>
2017-05-01 19:56:54 -0700

    modified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
-    browserSync = require('browser-sync');
-    eslint = require('gulp-eslint');
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync'),
+    eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require ('gulp-autoprefixer'),
+    cssnano =require ('gulp-cssnano'),
+    prettyerror = require ('gulp-prettyerror');
+
+gulp.task('sass', function(){
+  gulp.src('./sass/style.scss')
+    .pipe(prettyerror())
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css')) 
+});
 
 gulp.task('scripts',['lint'], function(){
   gulp.src('./js/*.js')
@@ -29,7 +45,7 @@ gulp.task('browser-sync', function(){
     }
   });
 
-  gulp.watch(['./build/js/*.js', './style.css']).on('change', browserSync.reload);
+  gulp.watch(['./build/js/*.js', './build/css/*.css']).on('change', browserSync.reload);
 });
 
 gulp.task('lint', function(){
@@ -38,6 +54,7 @@ gulp.task('lint', function(){
 
 gulp.task('watch', function(){
   gulp.watch('./js/*.js', ['scripts'])
-
+  gulp.watch('./sass/*.scss', ['sass'])
 });
+ 
 gulp.task('default', ['watch', 'browser-sync']);
--- js/main.js
+++ js/main.js
@@ -1,18 +1,80 @@
+$(function(){
+  var $articlelist = $('.article-list')
+  var $container = $('.container')
+  var $nytimage = $('.nyt-image')
+  var $preloader = $('.preloader')
+  var $navwrapper = $('.nav-wrapper')
+  var errortext = 'Sorry, there was an error, please try again'
+  
+    $preloader.hide();
+   
+   $('.sections').on('change', function () {
+     var userInput = $(this).find("option:selected").val();
+     if(userInput === "" ){
+       return
+     }
+     $container.addClass('header-shift-with-articles')
+     $nytimage.addClass('shrink-image')
+     $preloader.show();
+     $navwrapper.addClass('nav-article-load')
+     
+     
+     
+     var articleList = $('.article-list');
+     var url = 'https://api.nytimes.com/svc/topstories/v2/' + userInput + '.json';
+          url += '?' + $.param({
+          'api-key': "6f49b64835cc453b863bccd7ea3e593c"
+     }); //end of param
+     console.log(url); 
+    
 
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "6f49b64835cc453b863bccd7ea3e593c"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-  dataType:"json"
-}).done(function(result) {
-  $.each(data.results,
-  function(index, value){
-      article.length +='<li><img src="' + value.article+'">' + value.home+ '</li>'
-  });
-  console.log(result);
-}).fail(function(err) {
-  throw err;
-});
+
+
+   articleList.empty();
+   $.ajax({
+      url: url,
+      method: 'GET',
+    })//end of ajax method
+    .always(function () {
+        $preloader.hide();
+      })
+   .done(function (data) {
+        console.log(data);
+        var $data = data.results.filter(function (item) {
+          return item.multimedia.length;
+        console.log($data);
+        }).splice(0, 12);// end of function call back function
+   $.each($data, function (item, value) {
+          var fullArticle = '';
+          fullArticle += '<li class = "article-item">' + '<a href=';
+          fullArticle += value.url + '>';
+          fullArticle += '<div class ="text-container">' + '<p class = "content-text">';
+          fullArticle += value.abstract;
+          fullArticle += '</p>';
+          fullArticle += '<img class = "content-container"';
+          fullArticle += 'src="' + value.multimedia[4].url + '" />';
+          fullArticle += '</a>' + '</li>';
+          articleList.append(fullArticle);
+        });//end of each
+
+         })//end of done function
+      .fail(function () {
+        articleList.append(errortext);
+
+      });//beginning of fail function 
+  
+     
+
+
+
+
+   });
+
+
+
+}); //end of first function
+
+// right after ajax
+//data.result.filter(function(item){
+// return item.multimedia.length ? item : false;
+//})
